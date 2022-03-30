499068e3fd70c3f94cbf749ac6b4b8a352859882
Evgeniya <e.lytova@tinkoff.ru>
2022-03-30 18:57:30 +0300

    fix: поправила javadoc комментарий для TimestampMessageDecorator + название currentTime теперь с мал.буквы

--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -6,12 +6,13 @@ public class TimestampMessageDecorator {
 
     /**
      * this class changes an incoming message
-     * method 'TimestampMessageDecorator' adds current time to the beginning of the message. This method doesn't have any side effects
+     * method 'TimestampMessageDecorator' adds current time to the beginning of the message.
+     * this method doesn't have any side effects
      */
 
-    public static String CurrentTime = Instant.now().toString();
+    public static String currentTime = Instant.now().toString();
 
     public static String decorate(String message) {
-        return CurrentTime + ": " + message;
+        return currentTime + ": " + message;
     }
 }
