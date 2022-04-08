338a58e5276ccb0274e7610bc46d532857581f82
Evgeniya <e.lytova@tinkoff.ru>
2022-04-08 19:36:01 +0300

    feature: #5-2 : добавила постраничный вывод сообщений. перенесла определние номеров строк в декоратор

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,13 @@
 import com.tcs.edu.printer.ConsolePrinter;
 import com.tcs.edu.decorator.TimestampMessageDecorator;
 
+import static com.tcs.edu.decorator.TimestampMessageDecorator.messageCount;
+
 class Application {
     public static void main(String[] args) {
-        String message = TimestampMessageDecorator.decorate("Hello World!");
-        ConsolePrinter.print(message);
+        for (int i = 0; i < 6; i++) {
+            String message = TimestampMessageDecorator.decorate("Hello World!");
+            ConsolePrinter.print(message);
+        }
     }
 }
--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -7,14 +7,20 @@ public class TimestampMessageDecorator {
     /**
      * this class changes an incoming message
      * method 'TimestampMessageDecorator' adds current time to the beginning of the message.
-     * this method doesn't have any side effects
+     * this method doesn't have any side effects.
      */
 
-    public static Integer messageCount = 0;
+
     public static String currentTime = Instant.now().toString();
+    public static Integer messageCount = 0;
+    public static final Integer PAGE_SIZE = 2;
 
     public static String decorate(String message) {
-        final var decoratedMessage = currentTime + ": " + message;
+        ++messageCount;
+        var decoratedMessage = "[" + messageCount + "] " + currentTime + ": " + message;
+        if (messageCount % PAGE_SIZE == 0 && messageCount != 0) {
+            return decoratedMessage + "\n" +  "---";
+        }
         return decoratedMessage;
     }
 }
