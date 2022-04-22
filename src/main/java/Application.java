61fe48c6d87333c3f315121dc861b49757863f6d
Evgeniya <e.lytova@tinkoff.ru>
2022-04-22 23:49:40 +0300

    feature: #7.3 Добавлена перегруженная функция, которая умеет удалять дубликаты в переданном vararg

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,12 @@
 import com.tcs.edu.printer.MessageService;
-
 import static com.tcs.edu.enums.Severity.*;
 import static com.tcs.edu.enums.MessageOrder.*;
+import static com.tcs.edu.enums.Doubling.*;
 
 class Application {
     public static void main(String[] args) {
-        MessageService.print(MINOR, DESC, "Hello World!", "111", "333","222");
-        MessageService.print(REGULAR, ASC, "Hello World!", "444", "555");
+        MessageService.print(MINOR, DESC, DISTINCT, "Hello World!", "111", "222","222","333");
+        MessageService.print(REGULAR, ASC, DOUBLES, "Hello World!", "444", "555","555");
         MessageService.print(REGULAR, ASC, "Hello World!");
         MessageService.print(MAJOR, "Hello World!");
         MessageService.print(MINOR, "Hello World!");
