63a3cbb018d2f20bacd4d5c5f05cc91118dd2901
Evgeniya <e.lytova@tinkoff.ru>
2022-04-29 14:59:14 +0300

    fix: небольшой ренейминг классов

--- src/test/java/com/tinkoff/edu/Application.java
+++ src/test/java/com/tinkoff/edu/Application.java
@@ -1,23 +1,25 @@
 package com.tinkoff.edu;
 
 import com.tcs.edu.domain.Message;
-import com.tcs.edu.printer.MessageService;
+import com.tcs.edu.domain.MessageService;
+import com.tcs.edu.printer.OrderedDistinctedMessageService;
 import static com.tcs.edu.enums.Severity.*;
 import static com.tcs.edu.enums.MessageOrder.*;
 import static com.tcs.edu.enums.Doubling.*;
 
 class Application {
     public static void main(String[] args) {
-        MessageService callMessageService = new MessageService();
         Message message1 = new Message(DESC, DISTINCT, MINOR,"Hello World!", "22","22","33", "Hello World!");
         Message message2 = new Message(ASC, DOUBLES, REGULAR, "Hello World!", "444", "555","555");
         Message message3 = new Message(ASC, REGULAR, "Hello World!");
         Message message4 = new Message(MAJOR, "Hello World!");
         Message message5 = new Message("Hello World!");
-        callMessageService.log(message1);
-        callMessageService.log(message2);
-        callMessageService.log(message3);
-        callMessageService.log(message4);
-        callMessageService.log(message5);
+
+        MessageService service = new OrderedDistinctedMessageService();
+        service.log(message1);
+        service.log(message2);
+        service.log(message3);
+        service.log(message4);
+        service.log(message5);
     }
 }
