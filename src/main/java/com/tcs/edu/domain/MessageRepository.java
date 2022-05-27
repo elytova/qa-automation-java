de70e315279319da1616a770d3cc7a4e64f43a3e
Evgeniya <e.lytova@tinkoff.ru>
2022-05-27 10:42:12 +0300

    refactor: #11.3: добавила функцию findAll, которую вызываю из Application

--- src/main/java/com/tcs/edu/domain/MessageRepository.java
+++ src/main/java/com/tcs/edu/domain/MessageRepository.java
@@ -1,9 +1,12 @@
 package com.tcs.edu.domain;
 
+import java.util.Collection;
 import java.util.UUID;
 
 public interface MessageRepository {
     UUID create(Message message);
 
     Message findByPrimaryKey(UUID primaryKey);
+
+    Collection<Message> findAll();
 }
